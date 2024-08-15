8db24fbc1e4138d90c5eda92eb0bdc9f067b85a5
Igao2 <igor.dasilvacosta8@gmail.com>
2024-08-14 22:21:19 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var face = document.getElementById("face")
+var insta = document.getElementById("insta")
+face.addEventListener("click",function(){
+window.open("https://www.facebook.com/viviane.silvacosta.7","_blank")
+})
+insta.addEventListener("click",function(){
+    window.open("https://www.instagram.com/viviane.silvacosta.7/","_blank")
+})
